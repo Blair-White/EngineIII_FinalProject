ff7fb13396882d0835436b691aafae12d39b52a0
Blair-White <blairjwhite@hotmail.com>
2020-11-26 16:23:09 -0500

    Added Pause Menu and exit/resume

---
+++ Sem5_Eng3_Excercises-NewBlair/ExcercisesProject/Assets/_Scripts/System/PauseMenu.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class PauseMenu : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject oPauseMenu;
+    [SerializeField]
+    private bool isPaused;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            isPaused = !isPaused;
+        }
+
+        if(isPaused)
+        {
+            ActivateMenu();
+        }
+        else
+        {
+            DeactivateMenu();
+        }
+    }
+
+
+    void ActivateMenu()
+    {
+        Time.timeScale = 0;
+        oPauseMenu.SetActive(true);
+    }
+
+    void DeactivateMenu()
+    {
+        Time.timeScale = 1;
+        oPauseMenu.SetActive(false);
+    }
+
+    void PauseGame()
+    {
+        isPaused = !isPaused;
+    }
+
+    void ExitGame()
+    {
+        Application.Quit();
+    }
+}
